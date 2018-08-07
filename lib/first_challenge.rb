7fc8c0efa0e290a80db1def20ff5c6bbf9b8ae8f
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-07 18:00:27 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,7 @@ def first_challenge
  contacts.each do |person, data|
      data.each do |attribute, value|
        if attribute == :favorite_icecream_flavors
-      value.each do |flavor|
-        value.delete_if {|flavor| flavor=="strawberry"}
+     value.delete_if {|flavor| flavor=="strawberry"}
   puts "#{flavor}"
 end
 end
